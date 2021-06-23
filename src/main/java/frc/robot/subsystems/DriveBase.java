d82b3cbdfec2c3eb6fcf4d1520c856e6ea168580
Carlo Woolsey <cdbwoolsey@icloud.com>
2021-06-22 21:10:40 -0700

    Created DriveBase subsystem, ArcadeDrive command

---
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Talon;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+
+public class DriveBase extends SubsystemBase {
+  private final WPI_TalonSRX leftFrontMotor = new WPI_TalonSRX(Constants.leftFrontMotorPort);
+  private final WPI_TalonSRX leftBackMotor = new WPI_TalonSRX(Constants.leftBackMotorPort);
+  private final WPI_TalonSRX rightFrontMotor = new WPI_TalonSRX(Constants.rightFrontMotorPort);
+  private final WPI_TalonSRX rightBackMotor = new WPI_TalonSRX(Constants.rightBackMotorPort);
+  
+  private final SpeedControllerGroup leftMotors = new SpeedControllerGroup(leftFrontMotor, leftBackMotor);
+  private final SpeedControllerGroup rightMotors = new SpeedControllerGroup(rightFrontMotor,rightBackMotor);
+
+  private final DifferentialDrive differentialDrive = new DifferentialDrive(leftMotors, rightMotors);
+
+
+  /** Creates a new DriveBase. */
+  public DriveBase() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void ArcadeDrive(double speed, double rotation){
+    differentialDrive.arcadeDrive(speed, rotation);
+  }
+
+
+}
